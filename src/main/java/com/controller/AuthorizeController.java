221fa26e81797d0514be3a6390e57d78916adbf8
TDBryant <904900726@qq.com>
2022-08-19 11:23:44 +0800

    login

--- src/main/java/com/controller/AuthorizeController.java
+++ src/main/java/com/controller/AuthorizeController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 
 public class AuthorizeController {
@@ -27,7 +29,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -36,7 +39,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken =  githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        //System.out.println(user.getName());
+        if(user != null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+            //登录成功，写cookie和session
+        }
+        else{
+            return  "redirect:/";
+        }
     }
 }
--- src/test/java/com/ApplicationTests.java
+++ src/test/java/com/ApplicationTests.java
@@ -1,13 +1,35 @@
 package com;
 
+import com.alibaba.fastjson.JSON;
+import com.dto.AccessTokenDTO;
+import okhttp3.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("f4710ab19e53f4b4b73f");
+        accessTokenDTO.setClient_secret("e9408c14d640c32bc0dbd4387c0a7fab5c18c9b2");
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setCode("1");
+        accessTokenDTO.setState("2");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+        System.out.println(token);
     }
 
 }
